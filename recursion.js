4e7672b41ddced6e40037a05498037407d1ca403
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-10 13:12:09 -0400

    collects odd numbers

--- recursion.js
+++ recursion.js
@@ -1,4 +1,4 @@
-
+//super simple
 function sumRange(num){
     if(num === 1) return 1; 
     return num + sumRange(num-1);
@@ -6,7 +6,52 @@ function sumRange(num){
  
  sumRange(4)
 
+// with factorial
 function factorial(num){
     if(num === 1) return 1;
     return num * factorial(num-1);
 }
+
+// collecting odd numbers with a helper method
+function collectOddValues(arr){
+    
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+        
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7,8,9])
+
+// pure recursion
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+                                        
+                                           

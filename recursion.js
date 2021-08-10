49765e55080871aa4be71fd2d415a033d82785e0
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-10 12:29:11 -0400

    adds recursive functions

---
+++ recursion.js
@@ -0,0 +1,12 @@
+
+function sumRange(num){
+    if(num === 1) return 1; 
+    return num + sumRange(num-1);
+ }
+ 
+ sumRange(4)
+
+function factorial(num){
+    if(num === 1) return 1;
+    return num * factorial(num-1);
+}

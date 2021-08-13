cf142aff1137d844d5ac54c04e79e9a7e4005bfe
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-13 15:51:59 -0400

    adds swap toggle

--- bubbleSort.js
+++ bubbleSort.js
@@ -30,3 +30,29 @@ function bubbleSort(arr){
   }
   
   bubbleSort([8,1,2,3,4,5,6,7]);
+
+
+  /// SAVES TYPE WITH SWAPS TOGGLE!!!
+
+  function bubbleSort(arr){
+
+    const  swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    }
+    
+    for(var i = arr.length; i > 0; i--){
+      noSwaps = true;
+      for(var j = 0; j < i - 1; j++){
+        if(arr[j] > arr[j+1]){
+          swap(arr, j, j+1)
+          noSwaps = false;        
+        }
+      }
+      if(noSwaps) break;
+    }
+    return arr;
+  }
+  
+  
+  bubbleSort([8, 1, 20, 200])
+  

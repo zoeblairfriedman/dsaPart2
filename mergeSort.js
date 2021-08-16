dfa1718dc237338de334c38e9bbbd3fa03d8dbdf
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-16 12:09:15 -0400

    adds mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -23,5 +23,15 @@ function merge(arr1, arr2){
 }
 
 
-merge([1,10,50], [2,14,99,100])
+// merge([1,10,50], [2,14,99,100])
 
+function mergeSort(arr){
+    // split our array into smaller arrays
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+mergeSort([10,24, 76,73,72,1,9])

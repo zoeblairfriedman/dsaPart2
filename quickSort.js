d4c177adfabe9ad85866cfa435a35fd8e5a992ec
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-25 13:33:58 -0400

    adds quickSort function

--- quickSort.js
+++ quickSort.js
@@ -15,4 +15,17 @@ function pivot(arr, start=0, end=arr.length-1){
   return swapIdx;
   }
   
-  pivot([4,8,2,1,5,7,6,3])
+  
+  
+  function quickSort(arr, left=0, right=arr.length-1){
+      if (left < right) {
+          let pivotIdx = pivot(arr, left, right)
+          //left
+          quickSort(arr, 0, pivotIdx-1)
+          //right
+          quickSort(arr, pivotIdx+1,right)
+      }
+      return arr;
+  }
+  
+  quickSort([4,8,-5,2,1,5,7,6,3])

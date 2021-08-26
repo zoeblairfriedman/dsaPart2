993829e2d805b84841b5c349512dfa9a7ded9cd6
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-26 16:09:06 -0400

    adds radix sort

--- quickSort.js
+++ quickSort.js
@@ -1,5 +1,4 @@
 function pivot(arr, start=0, end=arr.length-1){
-
   const swap = (arr, idx1, idx2) => 
        ([arr[idx1],arr[idx2]]=[arr[idx2],arr[idx1]])
   
@@ -14,7 +13,6 @@ function pivot(arr, start=0, end=arr.length-1){
   swap(arr, start, swapIdx)
   return swapIdx;
   }
-  
   
   
   function quickSort(arr, left=0, right=arr.length-1){

170c12d5fe2cdc1808427ce77803d11560584a88
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-30 14:04:11 -0400

    refactored for recursion

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -24,13 +24,59 @@
 
 // how to create a private variable in javascript? 
 
-function secretVariable(){
-  var private = "super secret";
-  return function(){
-    return private;
+// function secretVariable(){
+//   var private = "super secret";
+//   return function(){
+//     return private;
+//   }
+// }
+
+// var getPrivate = secretVariable()
+
+// console.log(secretVariable())
+
+//naive solution
+// function scoreTracker(arr){
+//   let high = arr[0]
+//   let highCount = 0
+//   let low = arr[0]
+//   let lowCount = 0
+//   for (let i = 1; i < arr.length; i++){
+//       if (arr[i] > high) {
+//         high = arr[i];
+//         highCount ++;
+//       }
+//       if (arr[i] < low) {
+//         low = arr[i];
+//         lowCount ++;
+//       }
+      
+//   }
+//   console.log([highCount,lowCount])
+// }
+
+function scoreTracker(arr){
+  let high = arr[0]
+  let highCount = 0
+  let low = arr[0]
+  let lowCount = 0
+
+  const helper = (allScores) => {
+    allScores.shift();
+    if(allScores.length < 1) return;
+    if(allScores[0] > high){
+      high = allScores[0]
+      highCount++
+    }
+    if(allScores[0] < low){
+      low = allScores[0]
+      lowCount++
+    }
+    helper(allScores)
   }
+  helper(arr)
+  console.log([highCount, lowCount])
 }
 
-var getPrivate = secretVariable()
 
-console.log(secretVariable())
+scoreTracker([10, 5, 20, 20, 4, 5, 2, 25, 1])

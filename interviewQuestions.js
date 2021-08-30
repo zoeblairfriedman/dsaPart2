54e3742535957da30baa66f6a69350344cc01525
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-30 13:29:15 -0400

    adds spread operator, push, and unshift practice

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -1,7 +1,23 @@
-function gradingStudents(grades){
- grades.forEach(el => console.log( el < 38 || el % 5 <= 2 ? el : el + (5 - (el % 5))))
-}
+// function gradingStudents(grades){
+//  grades.forEach(el => console.log( el < 38 || el % 5 <= 2 ? el : el + (5 - (el % 5))))
+// }
 
-gradingStudents([73, 67, 38, 33])
-// rounding up to 5 IF within 2 points of that multiple
-// if rounding still means failing, don't round it 
+// gradingStudents([73, 67, 38, 33])
+// // rounding up to 5 IF within 2 points of that multiple
+// // if rounding still means failing, don't round it 
+
+// old school
+// function addToBeginning(input, array){
+//   array.unshift(input)
+//   console.log(array)
+// }
+// function addToEnd(input, array){
+//   array.push(input)
+//   console.log(array)
+// }
+
+//es6
+
+let myArray = ['a', 'b', 'c', 'd']
+myArray = ["start", ...myArray, "end"]
+console.log(myArray)

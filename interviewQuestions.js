e8694341dd0a6dbea4034d7cddac13e51a0c1f2f
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-08-30 13:19:59 -0400

    refactored for clarity

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -1,5 +1,5 @@
 function gradingStudents(grades){
- grades.forEach(el => console.log( el < 38 || el % 5 < 3 ? el : el + (5 - (el % 5))))
+ grades.forEach(el => console.log( el < 38 || el % 5 <= 2 ? el : el + (5 - (el % 5))))
 }
 
 gradingStudents([73, 67, 38, 33])

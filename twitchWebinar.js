38de20ad919477c74c00e0a1d9c25d446b07ccc7
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-01 17:22:48 -0400

    adds comments
    exit
    clera

--- twitchWebinar.js
+++ twitchWebinar.js
@@ -1,15 +1,18 @@
 //naive solution
 function missingWords(s, t){
 
+  // split both into an array
   let string = s.split(" ")
   let subString = t.split(" ")
-  let subObject = {}
 
+  // create a lookup object
+  let subObject = {}
   for (var i = 0; i < subString.length; i++){
     let word = subString[i]
     subObject[word] = 1;
   }
   
+  // filter out all the words that aren't here into an array
   console.log(string.filter(word => (!subObject[word])))
 }
 

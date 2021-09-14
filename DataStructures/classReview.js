c8d39e37764ad47866485687c4685326306ad763
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-14 14:47:29 -0400

    adds static class method

--- DataStructures/classReview.js
+++ DataStructures/classReview.js
@@ -6,10 +6,29 @@ class Student {
   constructor(firstName, lastName){
     this.firstName = firstName;
     this.lastName = lastName;
+    this.tardies = 0;
+    this.scores = [];
   }
   // here is an instance method
   greeting(){
     return `Hi! My name is ${this.firstName}!`
+  }
+  markLate(){
+    this.tardies++;
+    if (this.tardies >= 3){
+      return "YOU ARE EXPELLED!!!!"
+    }
+    return `${this.firstName} has been late ${this.tardies} times.`
+  }
+  addScore(score){
+    this.scores.push(score)
+    return this.scores
+  }
+
+  // class methods get the static keyword
+
+  static enrollStudents(...students){
+    // does something to all the students
   }
 
 }
@@ -17,4 +36,4 @@ class Student {
 let zoe = new Student("Zoe", "Friedman")
 let charlie = new Student("Charlie", "DeChants")
 
-console.log(zoe.greeting())
+console.log(zoe.markLate())

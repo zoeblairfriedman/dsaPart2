6346d8ebb16879acda20f4ee7d0deea46223bb18
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-16 17:19:47 -0400

    refactors for edge case

--- DataStructures/singlyLinkedList.js
+++ DataStructures/singlyLinkedList.js
@@ -36,6 +36,10 @@ class SinglyLinkedList{
     this.tail = newTail
     this.tail.next = null
     this.length--
+    if (this.length === 0){
+      this.head = null
+      this.tail = null
+    }
     return current
   }
   

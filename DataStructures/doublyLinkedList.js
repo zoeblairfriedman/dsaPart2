c6ff74b7850c55d2ae4d931e3c7b7075123ea9dc
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-11-29 12:59:09 -0500

    adds push for doubly linked list

--- DataStructures/doublyLinkedList.js
+++ DataStructures/doublyLinkedList.js
@@ -12,14 +12,27 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+
+  push(val){
+    let node = new Node(val)
+    if (!this.head) {
+      this.head = node
+      this.tail = node
+    } else {
+      this.tail.next = node
+      node.prev = this.tail
+      this.tail = node
+    }
+    this.length++;
+    return list;
+  }
 }
 
 
+let list = new DoublyLinkedList()
+list.push(13)
+list.push(14)
+list.push(15)
 
-
-
-let first = new Node(12)
-first.next = new Node(13)
-first.next.prev = first
-console.log(first.next.val)
 

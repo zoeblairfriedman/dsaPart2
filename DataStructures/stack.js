f068d67e75a6a4abefd115262885b6ddb513c5b0
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-12-02 12:07:14 -0500

    adds homemade stack

---
+++ DataStructures/stack.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let node = new Node(val)
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      var temp = this.first;
+      this.first = node;
+      this.first.next = temp
+    }
+    return ++this.size;
+  }
+
+  pop() { 
+  if (!this.first) return null;
+  let temp = this.first;
+  if(this.first === this.last) {
+    this.last = null;
+  } 
+  this.first = this.first.next
+  this.size--;
+  return temp.value
+}
+}
+
+let stack = new Stack()
+stack.push("instagram")
+stack.push("twitter")
+stack.push("facebook")
+console.log(stack.pop())
